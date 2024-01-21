d59dc7c471a6679af53afbf140045d02dc3bffde
CODE-SAMRAT <iamsamrat2056@gmail.com>
2024-01-21 17:22:09 +0545

    useTrackData hook craeted

--- src/helpers/hooks/useTrackData.ts
+++ src/helpers/hooks/useTrackData.ts
@@ -2,7 +2,5 @@ import { useQuery } from "react-query";
 import { GetAllTrackThunk } from "redux-app/inventory-module/InventorySlice";
 
 export const useTrackData = (id: number) => {
-   console.log(id,"id)
   return useQuery(["trackData", id], () => GetAllTrackThunk(id));
- 
 };
